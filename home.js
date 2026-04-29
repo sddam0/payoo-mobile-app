2d4a92a20dc3cf953caec83f5820b4596db55fc0
Saddam Hasan <saddamhasan0505@gmail.com>
2026-04-29 21:55:17 +0600

    Toggle functions and reuse function implements

--- home.js
+++ home.js
@@ -2,15 +2,46 @@
 // add money feature
 const validPin = 1234
 
+// function to get input values (reuse function create)
+function getInputValueNumber (id){
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    const inputFieldValueNumber = parseInt(inputFieldValue)
+    return inputFieldValueNumber
+
+}
+
+// without convert parsentInt converted
+function getInputValue (id){
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    return inputFieldValue
+}
+
+// function for inner text
+function getInnerText (id){
+    const element = document.getElementById(id)
+    const elementValue = element.innerText
+    const elementValueNumber = parseInt(elementValue)
+    return elementValueNumber
+
+}
+
+// function for total balance inner text
+function setInnerText(value){
+const avilableBalanceElement = document.getElementById("avilable-blance")
+avilableBalanceElement.innerText = value
+}
+
 document.getElementById("add-money-btn").addEventListener("click",function(e){
     e.preventDefault()
     console.log("add")
-    const bank = document.getElementById("bank").value 
+    const bank = getInputValue ("bank");
     const accountNumber = document.getElementById("account-number").value 
-    const amount = parseInt(document.getElementById("add-amount").value)
+    const amount = getInputValueNumber("add-amount")
     
-    const pin = parseInt(document.getElementById("add-pin").value) 
-    const avilableBalance = parseInt(document.getElementById("avilable-blance").innerText)
+    const pin = getInputValueNumber("add-pin")
+    const avilableBalance = getInnerText ("avilable-blance")
 
     if(accountNumber.length <11){
         alert("Please provide valid account number")
@@ -23,32 +54,66 @@ document.getElementById("add-money-btn").addEventListener("click",function(e){
     }
 
     const totalNewAvilableBalance = amount+avilableBalance
-    document.getElementById("avilable-blance").innerText = totalNewAvilableBalance
+
+    setInnerText(totalNewAvilableBalance)
+    
     
     
 })
+
+
+// function to toggle
+function handleToggle(id){
+
+    const forms = document.getElementsByClassName ("form")
+    for (const form of forms){
+        form.style.display = "none"
+    }
+document.getElementById(id).style.display = "block"
+
+}
+
 
 // cashout feture
 
 document.getElementById("withdraw-btn").addEventListener("click",function(e){
     e.preventDefault()
-    const amount = parseInt(document.getElementById("withdraw-amount").value)
-    const avilableBalance = parseInt( document.getElementById("avilable-blance").innerText)
+    const amount = getInputValueNumber("withdraw-amount")
+    const avilableBalance = getInnerText ("avilable-blance")
     const totalNewAvilableBalance = avilableBalance - amount
-    document.getElementById("avilable-blance").innerText = totalNewAvilableBalance
+    setInnerText(totalNewAvilableBalance)
 })
 
 
 
 // toggling features
 
-document.getElementById("add-button").addEventListener("click",function(){
-    document.getElementById("cash-out-parent").style.display = "none"
-    document.getElementById("add-money-parent").style.display = "block"
+document.getElementById("add-button").addEventListener("click",function(e){
+
+ handleToggle("add-money-parent")
+
+
 })
 document.getElementById("cash-out-button").addEventListener("click",function(){
-    document.getElementById("add-money-parent").style.display = "none"
-    document.getElementById("cash-out-parent").style.display = "block"
+   
+handleToggle("cash-out-parent")
     
 })
 
+document.getElementById("transfer-button").addEventListener("click", function(){
+    
+handleToggle("transfer-money-parent")
+})
+document.getElementById("bonus-button").addEventListener("click", function(){
+    
+handleToggle("get-bonus-money-parent")
+
+})
+document.getElementById("bil-button").addEventListener("click", function(){
+    
+handleToggle("pay-bil-money-parent")
+})
+document.getElementById("transaction-button").addEventListener("click", function(){
+
+handleToggle("transaction-money-parent")
+})
